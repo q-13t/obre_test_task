7dbb259fd464391364e3e54abfc35f2b1625bff9
q-13t <vova29092002@gmail.com>
2025-07-01 11:58:03 +0300

    IMP: UserContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,19 @@
+import { useEffect } from "react";
+import { UserContext } from "./hooks/user-context.tsx";
+import { useContext } from "react";
 
 
 function App() {
+    const user = useContext(UserContext);
+
+    // As if we perform login
+    useEffect(() => {
+        user.login({ id: 1, username: "q-13t", email: "temp@mail.org" });
+        return () => {
+            user.logout();
+        };
+    }, []);
+
     return (
         <div className="App">
             <h1 className="text-3xl font-bold underline">Hello</h1>
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import { UserContextProvider } from "./hooks/user-context.tsx";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <UserContextProvider >
+        <App />
+    </UserContextProvider>
+
 );
 
 
